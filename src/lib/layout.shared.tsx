173247bd41bd64581875725d711b98cdadeb5fa5
zacher <Saladegg24@gmail.com>
2026-03-08 09:32:15 +0700

    chore: updated github config

--- src/lib/layout.shared.tsx
+++ src/lib/layout.shared.tsx
@@ -1,10 +1,9 @@
 import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
 
-// fill this with your actual GitHub info, for example:
 export const gitConfig = {
   user: "zAcherttp",
   repo: "start-themes",
-  branch: "main",
+  branch: "master",
 };
 
 export function baseOptions(): BaseLayoutProps {
